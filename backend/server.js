7bf44eb1662442ba3f386765b381dcadda28af37
ahonanhin <yannelroy0@gmail.com>
2024-02-06 12:51:32 -0500

    modified:   frontend/src/Note.js
    i#

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,41 @@ app.post("/postNote", express.json(), async (req, res) => {
       insertedId: result.insertedId,
     });
 
+    if (data.matchedCount === 0) {
+      return res
+        .status(404)
+        .json({ error: "Unable to find note with given ID." });
+    }
+    res.json({ response: `Document with ID ${noteId} patched.` });
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+})
+
+
+app.patch('/updateNoteColor/:noteId', express.json(), async (req, res) => {
+  const { noteId } = req.params;
+  const { color } = req.body;
+
+  if (!ObjectId.isValid(noteId)) {
+      return res.status(400).json({ error: "Invalid note ID." });
+  }
+
+  try {
+      const collection = db.collection('notes');
+      await collection.updateOne({ _id: new ObjectId(noteId) }, { $set: { color } });
+      res.json({ message: 'Note color updated successfully.' });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/deleteAllNotes", express.json(), async (req, res) => {
+  try {
+    const collection = db.collection(COLLECTIONS.notes);
+    const data = await collection.deleteMany({});
+
+    res.json({ response: `${data.deletedCount} note(s) deleted.` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- frontend/src/Note.js
+++ frontend/src/Note.js
@@ -1,18 +1,25 @@
+
 import './App.css';
 
-const Note = ({entry, editNote, deleteNote}) => {
+const Note = ({entry, editNote, deleteNote, onChangeColor}) => {
+
+    const handleColorChange = (event) => {
+        const newColor = event.target.value;
+        onChangeColor(entry._id, newColor);
+    };
 
     const formatDate = (dateString) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
         return new Date(dateString).toLocaleDateString(undefined, options);
     };
 
-    return (
-        <div style={NoteStyle.note}>
+    return (   
+        <div style={{...NoteStyle.note, backgroundColor: entry.color || 'grey'}}>
             <span style={{ fontSize: '0.75rem', display: 'block', marginBottom: '5px' }}>
                 {formatDate(entry.createdAt)}
             </span>
             <p style={NoteStyle.text}>{entry.title}</p>
+                <input type="color" onChange={handleColorChange} value={entry.color || 'grey'} />
                 <button
                     onClick={() => editNote(entry)}
                     >
