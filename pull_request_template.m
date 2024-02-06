09bf5c1eb4d2564e63f531158dc21fbdd064c9f2
Jasjot S. Benipal <jasjot.benipal11@gmail.com>
2024-02-06 13:01:21 -0500

    Adding file for pull reuqest templates to root

---
+++ pull_request_template.m
@@ -0,0 +1,20 @@
+# Pull Request (PR) Template
+
+# Description of Changes
+Include a summary of change or issue fixed, and relevant information
+change to App.js and server.js that fixes deletion of a single note
+addition of colour coding feature for notes
+addition of feature that displays creation date of notes above note title, changes made in Note.js and server.js
+addition of feature that allows search for notes by title
+
+## Type of changes
+[ ] bug fix
+[ ] new feature
+
+## Checklist for Self Code Review
+ [ ] My code follows the style guidelines of this project
+ [ ] I have performed a self-review of my code
+ [ ] I have commented my code, particularly in hard-to-understand areas
+ [ ] I have recorded my changes in the deliverables folder, in the corresponding document
+ [ ] Ensure my changes cause no new warnings and have no impact on any unrelated features
+ [ ] Any dependent changes have been merged and published in downstream modules
